f4e919c6262abad4c7e8c2eda7ae41163a746ba3
AllanNara <allannara@outlook.com>
2024-07-15 04:38:10 -0300

    docs(docs): add documentation with swagger and use redocly/cli
    with yaml files and swagger-ui. Also refactoring code

--- src/config/swagger-config.js
+++ src/config/swagger-config.js
@@ -1,34 +1,21 @@
+import fs from "fs/promises";
 import path from "path";
-import swaggerJSDoc from "swagger-jsdoc";
 
-const swaggerOptions = {
-	definition: {
-		openapi: "3.0.0",
-		info: {
-			title: "E-Commercy API",
-			version: "beta 1.0.0",
-			description:
-				"Generic ecommerce API design for Backend course on Coderhouse, create by Allan Reynoso, Github /AllanNara",
-			contact: {
-				email: "allannara@outlook.com",
-				name: "Contact me",
-				url: "https://www.linkedin.com/in/allannara"
-			},
-			license: {
-				name: "MIT License",
-				url: "https://github.com/AllanNara/PF-Backend/blob/main/LICENSE.txt"
-			}
-		},
-		servers: [
-			{ url: "localhost:8080/api", description: "Local development API" }
-		],
-		tags: [
-			{ name: "carts", description: "CRUD operations of the user carts" },
-			{ name: "products", description: "CRUD operations of products" }
-		]
-	},
-	apis: [path.join("..", "..", "docs/**/*.yaml")]
-};
+let swaggerSpec;
 
-const swaggerSpec = swaggerJSDoc(swaggerOptions);
+try {
+	const path_apidoc = path.resolve(
+		import.meta.dirname,
+		"..",
+		"..",
+		"docs",
+		"openapi.json"
+	);
+	const json = await fs.readFile(path_apidoc, "utf-8");
+	swaggerSpec = JSON.parse(json);
+} catch (error) {
+	console.error({ openapi: `Error to read openapi.json: ${error}` });
+	throw new Error("Error reading file openapi.json");
+}
+
 export default swaggerSpec;
