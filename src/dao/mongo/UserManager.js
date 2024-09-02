9e4eb1030d235a7cbf7623f1f40730707daa5642
AllanNara <allannara@outlook.com>
2024-09-01 21:47:31 -0300

    refactor(dao): add messages if user not found

--- src/dao/mongo/UserManager.js
+++ src/dao/mongo/UserManager.js
@@ -18,7 +18,7 @@ export async function getUserById(uid) {
 		return user;
 	} catch (error) {
 		if (error?.message.includes("Cast to ObjectId failed")) {
-			logger.verbose("Product '%s' not found", uid);
+			logger.verbose("User with ID'%s' not found", uid);
 			return null;
 		}
 		throw error;
@@ -27,6 +27,9 @@ export async function getUserById(uid) {
 
 export async function getUserByEmail(email) {
 	const user = await userModel.findOne({ email });
-	if (!user) return null;
+	if (!user) {
+		logger.verbose("User '%s' not found", email);
+		return null;
+	}
 	return user;
 }
